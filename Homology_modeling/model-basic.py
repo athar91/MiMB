72110c801ab619b285e44d26d7cb590722ceaa67
Mohd Athar <102149843+athar91@users.noreply.github.com>
2024-04-16 22:08:29 +0200

    Add files via upload

---
+++ Homology_modeling/model-basic.py
@@ -0,0 +1,31 @@
+# Homology modeling by the automodel class 
+from modeller import *                   # Load standard Modeller classes 
+from modeller.automodel import *         # Load the automodel class
+
+seq="adeB"
+align="adeB-vs-acrB.ali"
+struct="2J8S.pdb"
+
+log.verbose()                            # request verbose output 
+env = environ()                          # create a new MODELLER environment to build this model in
+
+env.libs.topology.read(file='$(LIB)/top_heav.lib')  # H are not taken into calculations
+env.libs.parameters.read(file='$(LIB)/par.lib')
+
+# directories for input atom files 
+env.io.atom_files_directory = ['.', '../atom_files']
+
+a = automodel(env, 
+              # file with template codes and target sequence
+              alnfile=align, 
+              # PDB codes of the templates
+              knowns=struct,
+              # code of the target
+              sequence=seq,
+              # print dope score for models
+              assess_methods=(assess.DOPE))
+
+a.starting_model = 1
+a.ending_model = 100
+#a.auto_align() # get an automatic alignment
+a.make()       # do homolgy modeling
