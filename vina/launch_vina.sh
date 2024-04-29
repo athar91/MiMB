0edd95a9615d3129f73a31eb945b90547206e8a5
Mohd Athar <102149843+athar91@users.noreply.github.com>
2024-04-29 17:05:45 +0530

    Update launch_vina.sh

--- vina/launch_vina.sh
+++ vina/launch_vina.sh
@@ -1,4 +1,4 @@
-#Vina1.2.5 was obtained from conda (https://anaconda.org/conda-forge/vina)
+#This script has been tested for recent version of Vina v1.2.5, has been installed via conda (https://anaconda.org/conda-forge/vina)
 #conda activate vina
 source /$path/mgltools_x86_64Linux2_1.5.7/bin/mglenv.sh
 MGL_dir=/$path/mgltools_x86_64Linux2_1.5.7/MGLToolsPckgs/AutoDockTools/Utilities24
